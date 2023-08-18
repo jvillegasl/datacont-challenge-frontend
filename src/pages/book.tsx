ae6b449b9dd6e48c016d52d9f905d023008bcb3a
jvillegasl <jvillegasl@uni.pe>
2023-08-18 17:08:47 -0500

    wip: book page

---
+++ src/pages/book.tsx
@@ -0,0 +1,17 @@
+import { useBook } from "@/hooks";
+import { useParams } from "react-router-dom";
+
+export function Book() {
+    const { id } = useParams() as { id: string };
+    const { book, isLoading, hasFailed } = useBook(id);
+
+    return (
+        <>
+            {isLoading
+                ? "Loading..."
+                : hasFailed
+                ? "Failed"
+                : JSON.stringify(book)}
+        </>
+    );
+}
