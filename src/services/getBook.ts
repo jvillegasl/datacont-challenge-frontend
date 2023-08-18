6e66561dc12b4509c852835c4f5c761985e67694
jvillegasl <jvillegasl@uni.pe>
2023-08-18 17:09:08 -0500

    service getBook implemented

---
+++ src/services/getBook.ts
@@ -0,0 +1,14 @@
+import { IBook } from "@/interfaces";
+
+export function getBook(id: string) {
+    const endpoint =
+        import.meta.env.VITE_BACKEND_URL + "api/books/details/" + id;
+
+    return fetch(endpoint).then((data) => {
+        if (!data.ok) {
+            throw new Error(data.statusText);
+        }
+
+        return data.json() as Promise<IBook>;
+    });
+}
